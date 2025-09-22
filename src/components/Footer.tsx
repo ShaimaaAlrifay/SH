aecaa848bbee67b02b4bc3e4111ff04e1543be78
shaimaa alrifay <113690795+ShaimaaAlrifay@users.noreply.github.com>
2025-09-22 14:09:26 +0300

    fix: the links in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,9 +20,9 @@ export function Footer() {
   ];
 
   const socialLinks = [
-    { name: "GitHub", icon: Github, href: "https://github.com" },
-    { name: "LinkedIn", icon: Linkedin, href: "https://linkedin.com" },
-    { name: "Twitter", icon: Twitter, href: "https://twitter.com" },
+    { name: "GitHub", icon: Github, href: "https://github.com/ShaimaaAlrifay" },
+    { name: "LinkedIn", icon: Linkedin, href: "https://www.linkedin.com/in/shaimalrifay/" },
+    { name: "Twitter", icon: Twitter, href: "https://x.com/I1sh1" },
   ];
 
   const scrollToSection = (sectionId: string) => {
