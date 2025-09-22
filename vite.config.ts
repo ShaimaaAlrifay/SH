32074a9e531cba110d22a30a6f6049d958bdb392
shaimaa alrifay <113690795+ShaimaaAlrifay@users.noreply.github.com>
2025-09-22 11:45:43 +0300

    install some pakages and fix the index file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/SH/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -58,5 +59,4 @@
       port: 3000,
       open: true,
     },
-    base: '/SH/',
   });
